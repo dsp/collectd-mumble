48e02c8ff55da51826a503737c35cc247defccbc
David Soria Parra <dsp@php.net>
2016-05-28 07:24:11 +0200

    clang format --style=llvm

--- src/collector.cpp
+++ src/collector.cpp
@@ -4,63 +4,60 @@
 #include <iostream>
 #include <string>
 
-MumbleCollector::MumbleCollector(
-            std::string _host,
-            uint32_t _port,
-            std::string _secret) {
-    auto props = Ice::createProperties();
-    props->setProperty("Ice.ImplicitContext", "Shared");
+MumbleCollector::MumbleCollector(std::string _host, uint32_t _port,
+                                 std::string _secret) {
+  auto props = Ice::createProperties();
+  props->setProperty("Ice.ImplicitContext", "Shared");
 
-    Ice::InitializationData idata;
-    idata.properties = props;
+  Ice::InitializationData idata;
+  idata.properties = props;
 
-    this->ice = Ice::initialize(idata);
-    this->port = _port;
-    this->host = _host;
-    this->secret = _secret;
+  this->ice = Ice::initialize(idata);
+  this->port = _port;
+  this->host = _host;
+  this->secret = _secret;
 }
 
-std::string MumbleCollector::connectStr(
-        ) {
-    std::ostringstream stringStream;
-    stringStream << "Meta:tcp ";
-    stringStream << "-h " << this->host << " ";
-    stringStream << "-p " << this->port << " ";
-    stringStream << "-t 1000";
-    return stringStream.str();
+std::string MumbleCollector::connectStr() {
+  std::ostringstream stringStream;
+  stringStream << "Meta:tcp ";
+  stringStream << "-h " << this->host << " ";
+  stringStream << "-p " << this->port << " ";
+  stringStream << "-t 1000";
+  return stringStream.str();
 }
 
 /**
- * Connect to the Mumble server via the ICE protocl and initialize a Meta class which is then
- * returned. This meta class can be used to further examine the data on the server, such as enumerating
+ * Connect to the Mumble server via the ICE protocl and initialize a Meta class
+ * which is then
+ * returned. This meta class can be used to further examine the data on the
+ * server, such as enumerating
  * servers.
  */
-Murmur::MetaPrx MumbleCollector::connect(
-        ) {
+Murmur::MetaPrx MumbleCollector::connect() {
 
-    if (this->secret.size() > 0) {
-        this->ice->getImplicitContext()->put("secret", this->secret);
-    }
+  if (this->secret.size() > 0) {
+    this->ice->getImplicitContext()->put("secret", this->secret);
+  }
 
-    auto obj = this->ice->stringToProxy(this->connectStr());
-    auto meta = Murmur::MetaPrx::checkedCast(obj);
-    if (!meta) {
-        return nullptr;
-    }
-    return meta;
+  auto obj = this->ice->stringToProxy(this->connectStr());
+  auto meta = Murmur::MetaPrx::checkedCast(obj);
+  if (!meta) {
+    return nullptr;
+  }
+  return meta;
 }
 
-MumbleCollector::~MumbleCollector(
-        ) {
-    if (this->ice) {
-        this->ice->destroy();
-    }
+MumbleCollector::~MumbleCollector() {
+  if (this->ice) {
+    this->ice->destroy();
+  }
 }
 
 uint32_t MumbleCollector::getUserCount() {
-    auto meta = this->connect();
-    for (auto server : meta->getBootedServers()) {
-        return static_cast<uint32_t>(server->getUsers().size());
-    }
-    return 0;
+  auto meta = this->connect();
+  for (auto server : meta->getBootedServers()) {
+    return static_cast<uint32_t>(server->getUsers().size());
+  }
+  return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #ifdef HAVE_COLLECTD
 extern "C" {
- #define HAVE_ENDIAN_H 1
- #include <endian.h>
- #include <stdint.h>
- #include <stdbool.h>
- #include <collectd/liboconfig/oconfig.h>
- #include <collectd/core/daemon/collectd.h>
- #include <collectd/core/daemon/common.h> /* auxiliary functions */
- #include <collectd/core/daemon/plugin.h> /* plugin_register_*, plugin_dispatch_values */
+#define HAVE_ENDIAN_H 1
+#include <endian.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <collectd/liboconfig/oconfig.h>
+#include <collectd/core/daemon/collectd.h>
+#include <collectd/core/daemon/common.h> /* auxiliary functions */
+#include <collectd/core/daemon/plugin.h> /* plugin_register_*, plugin_dispatch_values */
 }
 #endif
 
@@ -20,94 +20,84 @@ extern "C" {
 // static const char * PLUGIN_NAME = "mumble";
 #define PLUGIN_NAME "mumble"
 
-static const char *config_keys[] =
-{
-    "IceHost",
-    "IcePort",
-    "IceSecret"
-};
+static const char *config_keys[] = {"IceHost", "IcePort", "IceSecret"};
 static std::string ice_host = "";
 static std::string ice_secret = "";
 static uint32_t ice_port = 6502;
 static int config_keys_num = sizeof(config_keys);
 
-MumbleCollector* collector = nullptr;
+MumbleCollector *collector = nullptr;
 
 #ifdef HAVE_COLLECTD
-static int my_config (const char *key, const char *value)
-{
-    if (strcmp(key,"IcePort") == 0) {
-        auto mport = strtol(key, nullptr, 10);
-        assert(mport > 0 && mport < 65536);
-        ice_port = static_cast<uint32_t>(mport);
-        return 0;
-    }
+static int my_config(const char *key, const char *value) {
+  if (strcmp(key, "IcePort") == 0) {
+    auto mport = strtol(key, nullptr, 10);
+    assert(mport > 0 && mport < 65536);
+    ice_port = static_cast<uint32_t>(mport);
+    return 0;
+  }
 
-    if (strcmp(key, "IceHost") == 0) {
-        ice_host = std::string(value);
-        return 0;
-    }
+  if (strcmp(key, "IceHost") == 0) {
+    ice_host = std::string(value);
+    return 0;
+  }
 
-    if (strcmp(key, "IceSecret") == 0) {
-        ice_secret = std::string(value);
-        return 0;
-    }
-    return 1;
+  if (strcmp(key, "IceSecret") == 0) {
+    ice_secret = std::string(value);
+    return 0;
+  }
+  return 1;
 }
 
-static int my_init (void)
-{
-    collector = new MumbleCollector(ice_host, ice_port, ice_secret);
-    return 0;
-}
- 
-static int my_shutdown(void)
-{
-    if (collector) {
-        delete collector;
-    }
-    return 0;
+static int my_init(void) {
+  collector = new MumbleCollector(ice_host, ice_port, ice_secret);
+  return 0;
 }
 
-static int my_read (void)
-{
-    value_t values[1];
-    value_list_t vl = VALUE_LIST_INIT;
+static int my_shutdown(void) {
+  if (collector) {
+    delete collector;
+  }
+  return 0;
+}
 
-    values[0].gauge = collector->getUserCount();
+static int my_read(void) {
+  value_t values[1];
+  value_list_t vl = VALUE_LIST_INIT;
 
-    vl.values = values;
-    vl.values_len = STATIC_ARRAY_SIZE(values);
-    sstrncpy (vl.host, hostname_g, sizeof (vl.host));
-    sstrncpy (vl.plugin, PLUGIN_NAME, sizeof (vl.plugin));
-    sstrncpy (vl.type, "count", sizeof (vl.type));
-    plugin_dispatch_values (&vl);
-    return 0;
+  values[0].gauge = collector->getUserCount();
+
+  vl.values = values;
+  vl.values_len = STATIC_ARRAY_SIZE(values);
+  sstrncpy(vl.host, hostname_g, sizeof(vl.host));
+  sstrncpy(vl.plugin, PLUGIN_NAME, sizeof(vl.plugin));
+  sstrncpy(vl.type, "count", sizeof(vl.type));
+  plugin_dispatch_values(&vl);
+  return 0;
 }
 
 /**
  * Definition for the datatype to be used by collectd
  */
-static data_source_t mumble_dsrc[] = {{"count", DS_TYPE_GAUGE, 0, NAN }};
-static data_set_t mumble_ds = {PLUGIN_NAME, STATIC_ARRAY_SIZE(mumble_dsrc), mumble_dsrc};
+static data_source_t mumble_dsrc[] = {{"count", DS_TYPE_GAUGE, 0, NAN}};
+static data_set_t mumble_ds = {PLUGIN_NAME, STATIC_ARRAY_SIZE(mumble_dsrc),
+                               mumble_dsrc};
 
 extern "C" {
-    void module_register ()
-    {
-        plugin_register_data_set(&mumble_ds);
-        plugin_register_config (PLUGIN_NAME, my_config, config_keys, config_keys_num);
-        plugin_register_init (PLUGIN_NAME, my_init);
-        plugin_register_shutdown (PLUGIN_NAME, my_shutdown);
-        plugin_register_read (PLUGIN_NAME, my_read);
-    }
+void module_register() {
+  plugin_register_data_set(&mumble_ds);
+  plugin_register_config(PLUGIN_NAME, my_config, config_keys, config_keys_num);
+  plugin_register_init(PLUGIN_NAME, my_init);
+  plugin_register_shutdown(PLUGIN_NAME, my_shutdown);
+  plugin_register_read(PLUGIN_NAME, my_read);
+}
 }
 
 #else
 
-int main (int argc,char**argv) {
-    MumbleCollector collector;
-    std::cout << collector.getUserCount() << std::endl;
-    return 0;
+int main(int argc, char **argv) {
+  MumbleCollector collector;
+  std::cout << collector.getUserCount() << std::endl;
+  return 0;
 }
 #endif
-
